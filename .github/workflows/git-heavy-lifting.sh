c5d09b7ee72c0f66c4b557a7c867320cd80af144
Krzesimir Nowak <krzesimir@kinvolk.io>
2020-06-23 14:19:22 +0200

    fix directory check

--- .github/workflows/git-heavy-lifting.sh
+++ .github/workflows/git-heavy-lifting.sh
@@ -24,7 +24,7 @@ fi
 
 repodir="${workdir}/repos/${owner}/${repo}"
 
-if [[ -d "${repodir}" ]]; then
+if [[ ! -d "${repodir}" ]]; then
     mkdir -p "$(dirname "${repodir}")"
     git clone "https://github.com/${owner}/${repo}.git" "${repodir}"
 else
