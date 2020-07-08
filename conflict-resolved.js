f46e62ec22eb3378b7f7736491c1caf36257b468
Krzesimir Nowak <krzesimir@kinvolk.io>
2020-07-08 19:19:10 +0200

    use the refactored command parsing

--- conflict-resolved.js
+++ conflict-resolved.js
@@ -15,54 +15,45 @@ module.exports = ({context, github, io, core}) => {
             const scriptPath = path.resolve('./file-propagation-pr.js')
             return require(scriptPath)
         })()
+        let parse_commands = (() => {
+            const path = require('path')
+            const scriptPath = path.resolve('./parse-commands.js')
+            return require(scriptPath)
+        })()
         const { data: comment } = await github.issues.getComment({
             owner: config.central_repo_owner,
             repo: config.central_repo_repo,
             comment_id: context.payload.comment.id,
         })
-        const lines = comment.body.split("\n")
-        const prefix = `@${config.bot_name}:`
-        const errors = []
-        const messages = []
-        let branch = ""
-        lines_loop:
-        for (let line of lines) {
-            line = line.trim()
-            if (!line.startsWith(prefix)) {
-                continue
-            }
-            line = line.slice(prefix.length).trim()
-            const [cmd, ...rest] = line.split(/\s+/)
-            if (cmd == "resolved") {
-                if (rest.length !== 1) {
-                    errors.push(`Expected one parameter for the "resolved" command being a name of a branch in "${line}"`)
-                    continue
-                }
-                if (branch.length !== 0) {
-                    errors.push(`The branch with resolved conflict is already specified ("${branch}")`)
-                    continue
-                }
-                branch = rest[0]
-                continue
-            }
-            erros.push(`Unknown command "${cmd}". The only available command is "resolved <branch_name>".`)
+        const result = parse_commands({
+            body: comment.body,
+            bot_name: config.bot_name,
+        })
+        if (result.cmd_data.propagation_status !== "?") {
+            result.errors.push("Propagation commands are ignored in this context")
         }
-        if (errors.length > 0) {
-            branch = ""
+        if (result.closings.length > 0) {
+            result.errors.push("Closing commands are ignored in this context")
+        }
+        if (result.errors.length > 0) {
             // TODO: create a comment with errors
+            throw 42
         }
-        if (branch !== "") {
-            console.log("context:", context)
-            const issue_number = context.payload.issue.number
-            let result = parse_propagation_issue({body: context.payload.issue.body})
-            if (result.errors.length > 0) {
-                for (let error of result.errors) {
-                    console.log(error)
-                }
-                return
+        if (result.cmd_data.resolve_branch !== "") {
+            let result2 = parse_propagation_issue({
+                body: context.payload.issue.body
+            })
+            if (result2.errors.length > 0) {
+                // TODO: create a comment with errors
+                throw 42
             }
-            let pr_data = result.pr_data
-            await file_propagation_pr({github, config, pr_data, head_branch: branch, issue_number})
+            await file_propagation_pr({
+                github: github,
+                config: config,
+                pr_data: result2.pr_data,
+                head_branch: result.cmd_data.resolve_branch,
+                issue_number: context.payload.issue.number,
+            })
         }
     })()
 }
--- pr-merged.js
+++ pr-merged.js
@@ -10,6 +10,17 @@ module.exports = ({context, github}) => {
             const scriptPath = path.resolve('./pr-data-to-issue-body.js')
             return require(scriptPath)
         })()
+        const parse_flatcar_commands = (() => {
+            const path = require('path')
+            const scriptPath = path.resolve('./parse-flatcar-commands.js')
+            return require(scriptPath)
+        })()
+        const get_flatcar_branches = (() => {
+            const path = require('path')
+            const scriptPath = path.resolve('./get-flatcar-branches.js')
+            return require(scriptPath)
+        })()
+
         try {
             await github.pulls.checkIfMerged({
                 owner: context.repo.owner,
@@ -20,216 +31,26 @@ module.exports = ({context, github}) => {
             console.log(`PR closed, skipping`, error)
             return
         }
-        let time_desc_re = /^\s*(\d+)([wdh])\s*$/
-        let date_desc_re = /^\s*((\d{4})-(\d{1,2})-(\d{1,2}))\s*$/
-        let issue_number_re = /^\s*(\d+)\s*$/
-        // parse body for commands
-        const body = context.payload.pull_request.body
         const { data: pr } = await github.pulls.get({
             owner: context.repo.owner,
             repo: context.repo.repo,
             pull_number: context.payload.pull_request.number,
         })
-        let s2l_branch_map = {
-            "alpha": "flatcar-master-alpha",
-            "beta": "flatcar-master-beta",
-            "stable": "flatcar-master",
-        }
-        let l2s_branch_map = {
-            "flatcar-master-alpha": "alpha",
-            "flatcar-master-beta": "beta",
-            "flatcar-master": "stable",
-        }
-        let propagate_branches = {}
-        for (let key in s2l_branch_map) {
-            propagate_branches[key] = {
-                "available": false, // does the project have this branch?
-                "allowed": false, // is it allowed to propagate to this branch?
-                "specified": false, // was it already specified in the bot command?
-            }
-        }
-        {
-            const per_page = 100
-            let page = 0
-            while (1) {
-                // page numbering is 1-based, so we increment it before doing the call
-                page++
-                const { data: branches } = await github.repos.listBranches({
-                    owner: context.repo.owner,
-                    repo: context.repo.repo,
-                    page: page,
-                    per_page: per_page,
-                })
-                for (let branch of branches) {
-                    if (branch.name in l2s_branch_map) {
-                        propagate_branches[l2s_branch_map[branch.name]].available = true
-                    }
-                }
-                if (branches.length < per_page) {
-                    break
-                }
-            }
-        }
-        const target_branch = pr.base.ref
-        switch (target_branch) {
-        case "flatcar-master-edge":
-            propagate_branches["alpha"].allowed = true
-            // fallthrough
-        case "flatcar-master-alpha":
-            propagate_branches["beta"].allowed = true
-            // fallthrough
-        case "flatcar-master-beta":
-            propagate_branches["stable"].allowed = true
-            // fallthrough
-        }
-        const lines = body.split("\n")
-        // @<bot>: propagate branch_desc date_spec
-        // @<bot>: no-propagate
-        // @<bot>: close issue_number
-        //
-        // branch_desc: alpha, beta, stable
-        // date_spec: nope, asap, \d+[mwd] (month, week, day), yyyy-mm-dd
-        // issue_number: \d+
-        const prefix = `@${config.bot_name}:`
-        let issues = {
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            pr: context.payload.pull_request.number,
-            branches: [], // { name: , date: , }
-            commits: [],
-        }
-        let closings = []
-        const ps_unknown = 0
-        const ps_no = 1
-        const ps_yes = 2
-        let propagation_status = ps_unknown
-        let propagation_mixed_warned = false
-        for (let line of lines) {
-            if (!line.startsWith(prefix)) {
-                continue
-            }
-            line = line.slice(prefix.length)
-            line = line.trim()
-            const [cmd, ...rest] = line.split(/\s+/)
-            if (cmd === "no-propagate") {
-                if (rest.length !== 0) {
-                    console.log('invalid "no-propagate" command')
-                    continue
-                }
-                if (propagation_status === ps_yes) {
-                    if (!propagation_mixed_warned) {
-                        console.log("mixed propagation commands")
-                        propagation_mixed_warned = true
-                    }
-                    continue
-                }
-                propagation_status = ps_no
-                continue
-            }
-            if (cmd === "close") {
-                if (rest.length !== 1) {
-                    console.log(`close command invalid`)
-                    continue
-                }
-                let match = rest[0].match(issue_number_re)
-                if (match === null || match.length !== 2) {
-                    console.log(`"${issue_number}" in close command is not a number`)
-                    continue
-                }
-                const issue_number = match[1]
-                closings.push(issue_number)
-                continue
-            }
-            if (cmd === "propagate") {
-                if (propagation_status === ps_no) {
-                    if (!propagation_mixed_warned) {
-                        console.log("mixed propagation commands")
-                        propagation_mixed_warned = true
-                    }
-                    continue
-                }
-                propagation_status = ps_yes
-                const periods = rest.join(" ").split(",")
-                for (let period of periods) {
-                    period = period.trim()
-                    const words = period.split(/\s+/)
-                    if (words.length !== 2) {
-                        console.log(`${period} invalid`)
-                        continue
-                    }
-                    const branch_desc = words[0].trim()
-                    if (!(branch_desc in s2l_branch_map)) {
-                        console.log(`${branch_desc} invalid`)
-                        continue
-                    }
-                    if (!propagate_branches[branch_desc].available) {
-                        console.log(`${branch_desc} unavailable`)
-                        continue
-                    }
-                    if (!propagate_branches[branch_desc].allowed) {
-                        console.log(`${branch_desc} not allowed`)
-                        continue
-                    }
-                    if (propagate_branches[branch_desc].specified) {
-                        console.log(`${branch_desc} already specified`)
-                        continue
-                    }
-                    propagate_branches[branch_desc].specified = true
-                    const time_desc = words[1].trim()
-                    let date = new Date()
-                    date = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 12)
-                    if (time_desc === "asap") {
-                        // TODO: file PRs immediately?
-                    } else if (time_desc === "nope") {
-                        console.log(`${branch_desc} not to be propagated`)
-                        continue
-                    } else {
-                        let match = time_desc.match(time_desc_re)
-                        if (match === null) {
-                            match = time_desc.match(date_desc_re)
-                            if (match === null || match.length !== 5) {
-                                console.log(`${time_desc} matched no regexps`)
-                                continue
-                            }
-                            const year = parseInt(match[2], 10)
-                            const month = parseInt(match[3], 10)
-                            const day = parseInt(match[4], 10)
-                            // months are zero-based in Date, but we
-                            // use 1-based in our messages
-                            date = new Date(year, month-1, day, 12)
-                            if ((date.getFullYear() !== year) || (date.getMonth() !== month-1) || (date.getDate() !== day)) {
-                                console.log(`${time_desc} has bogus date (actually ${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()})`)
-                                continue
-                            }
-                        } else {
-                            if (match.length !== 3) {
-                                console.log(`${time_desc} short desc invalid?`)
-                                continue
-                            }
-                            switch (match[2]) {
-                            case "m":
-                                date.setMonth(date.getMonth() + parseInt(match[1], 10))
-                                break
-                            case "w":
-                                date.setDate(date.getDate() + parseInt(match[1], 10) * 7)
-                                break
-                            case "d":
-                                date.setDate(date.getDate() + parseInt(match[1], 10))
-                                break
-                            }
-                        }
-                    }
-                    issues.branches.push({
-                        name: s2l_branch_map[branch_desc],
-                        date: date,
-                    })
-                }
-                continue
-            }
-        }
-        if (issues.branches.length > 0) {
+        const flatcar_branches = await get_flatcar_branches({
+            github: github,
+            context: context,
+            config: config,
+        })
+        let result = parse_flatcar_commands({
+            body: context.payload.pull_request.body,
+            config: config,
+            target_branch: pr.base.ref,
+            branches_set: flatcar_branches,
+        })
+        if (result.cmd_data.propagation_status === "yes") {
             let page = 0
             let per_page = 100
+            let pr_commits = []
             while (1) {
                 // page numbering is 1-based, so we increment it
                 // before doing the call
@@ -244,30 +65,35 @@ module.exports = ({context, github}) => {
                 // TODO: I'm not sure if this returns commits sorted
                 // by parents.
                 for (let commit of commits) {
-                    issues.commits.push(commit.sha)
+                    pr_commits.push(commit.sha)
                 }
                 if (commits.length < per_page) {
                     break
                 }
             }
             const pr_title = pr.title.trim().replace(/\n/g, " ")
-            for (let branch of issues.branches) {
+            for (let prop_branch of result.cmd_data.propagation_branches) {
+                if (prop_branch.date === null) {
+                    continue
+                }
                 let pr_data = {
-                    owner: issues.owner,
-                    repo: issues.repo,
-                    pr: issues.pr,
-                    branch: branch.name,
+                    owner: context.repo.owner,
+                    repo: context.repo.repo,
+                    pr: context.payload.pull_request.number,
+                    branch: prop_branch.name,
                     filed_pr_url: "",
                     card_id: 0,
-                    date: branch.date,
+                    date: prop_branch.date,
                     title: pr_title,
-                    commits: issues.commits,
+                    commits: pr_commits,
                 }
                 const { data: issue } = await github.issues.create({
                     owner: config.central_repo_owner,
                     repo: config.central_repo_repo,
                     title: `For ${branch.name}: ${pr.title}`,
-                    body: pr_data_to_issue_body({pr_data}),
+                    body: pr_data_to_issue_body({
+                        pr_data: pr_data,
+                    }),
                 })
                 await github.projects.createCard({
                     column_id: config.central_pending_column_id,
@@ -276,7 +102,7 @@ module.exports = ({context, github}) => {
                 })
             }
         }
-        for (let issue_number of closings) {
+        for (let issue_number of result.cmd_data.closings) {
             await github.issues.update({
                 owner: config.central_repo_owner,
                 repo: config.central_repo_repo,
