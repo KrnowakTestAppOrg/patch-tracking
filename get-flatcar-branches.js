6a3aebc76eeaae52bbbd0341875c3dcfced94d3f
Krzesimir Nowak <krzesimir@kinvolk.io>
2020-07-08 19:12:33 +0200

    add flatcar branches getter

---
+++ get-flatcar-branches.js
@@ -0,0 +1,29 @@
+module.exports = async ({github, context, config}) => {
+    let all_flatcar_branches_set = {}
+    for (let [short_name, full_name] of config.short_to_full_branch_map) {
+        all_flatcar_branches_set[full_name] = {}
+    }
+    let available_flatcar_branches_set = {}
+    const per_page = 100
+    let page = 0
+    while (1) {
+        // page numbering is 1-based, so we increment it
+        // before doing the call
+        page++
+        const { data: branches } = await github.repos.listBranches({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            page: page,
+            per_page: per_page,
+        })
+        for (let branch of branches) {
+            if (branch.name in all_flatcar_branches_set) {
+                available_flatcar_branches[branch.name] = true
+            }
+        }
+        if (branches.length < per_page) {
+            break
+        }
+    }
+    return available_flatcar_branches_set
+}
